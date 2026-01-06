2ae85c31062a42ac0ba941ba677f4fe3dae22d7b
unawarexi <unawarexi@gmail.com>
2026-01-06 10:05:21 +0100

    updated

--- Frontend-Unidel-CBTsoftware/src/pages/admin/presentation/users-management/Students-management.jsx
+++ Frontend-Unidel-CBTsoftware/src/pages/admin/presentation/users-management/Students-management.jsx
@@ -325,7 +325,7 @@ const StudentsManagement = () => {
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
               exit={{ opacity: 0 }}
-              className="fixed inset-0 bg-black/50 flex items-center justify-center p-4 z-50"
+              className="fixed inset-0 bg-black/50 flex items-center justify-center p-4 z-40"
               onClick={() => {
                 setShowModal(false);
                 setEditingStudent(null);
